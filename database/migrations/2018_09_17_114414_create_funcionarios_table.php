c7387d07f78ed80d794ae03d23fbba789daded8e
May <alvaresbagli@gmail.com>
2018-10-29 19:50:48 -0300

    projeto oficial

---
+++ database/migrations/2018_09_17_114414_create_funcionarios_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFuncionariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('funcionarios', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome');
+            $table->string('email');
+            $table->string('endereco');
+            $table->string('cpf');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('funcionarios');
+    }
+}
---
+++ database/migrations/2018_09_17_115441_create_servico_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateServicoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('servicos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('descricao');
+            $table->integer('dispositivo')->references('dispositivos')->on('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('servico');
+    }
+}
