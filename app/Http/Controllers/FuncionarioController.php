1e9e5ba839a1633094534bc6a80364fb3c79f6ce
may <alvaresbagli@gmail.com>
2018-11-20 12:07:07 -0200

    form-issue

--- app/Http/Controllers/FuncionarioController.php
+++ app/Http/Controllers/FuncionarioController.php
@@ -9,10 +9,6 @@ use App\Http\Requests;
 class FuncionarioController extends Controller
 {
 
-    public function __construct()
-    {
-        //$this->middleware('auth');
-    } 
     public function index()
     {
         return view('funcionarios.lista');
