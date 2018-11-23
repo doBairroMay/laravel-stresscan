b33322ac45d8aaa73971dd023fbc11ab1788147d
may <alvaresbagli@gmail.com>
2018-11-23 16:13:28 -0200

    agora foi

--- resources/views/funcionarios/form.blade.php
+++ resources/views/funcionarios/form.blade.php
@@ -1,5 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container spark-screen">
+	<div class="row">
+		<div class="col-md-10 col-md-offset-1">
+			<div class="panel-heading">
+			  Informações do Cliente
+        <a class="pull-right" href="{{ url('clientes') }}"> Listagem de Clientes</a>
+       </div>
+        <div class="panel-body">
+          {!!Form::open()!!}
+          {!! Form::input('text','nome', '')}
+          {!!Form::close()!!}
+        </div>
 
-@stop
+     </div>
+    </div>
+  </div>
+</div>
+@endsection
