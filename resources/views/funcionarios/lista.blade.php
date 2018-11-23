3031a715df8d06cc8fb992d1ec1811f86b2d0130
may <alvaresbagli@gmail.com>
2018-11-23 15:57:36 -0200

    laravel-css

--- resources/views/funcionarios/lista.blade.php
+++ resources/views/funcionarios/lista.blade.php
@@ -1,154 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="content">
-    <form class="navbar-form col-md-8">
-              <span class="bmd-form-group"><div class="input-group no-border">
-                <input type="text" value="" class="form-control" placeholder="Search...">
-                <button type="submit" class="btn btn-white btn-round btn-just-icon">
-                  <i class="material-icons">search</i>
-                  <div class="ripple-container"></div>
-                </button>
-              </div></span>
-     </form>
-        <div class="container-fluid">
-          <div class="row">
-            <div class="col-md-12">
-              <div class="card">
-                <div class="card-header card-header-primary">
-                  <h3 class="card-title ">Lista de funcionários</h3>
-                  <h4 class="card-category"><a href="{{ url('/funcionarios/novo') }}">Adicionar Funcionário</a></h4>
-                </div>
-                <div class="card-body">
-                  <div class="table-responsive">
-                    <table class="table">
-                      <thead class=" text-primary">
-                        <tr><th>
-                          ID
-                        </th>
-                        <th>
-                          Name
-                        </th>
-                        <th>
-                          Country
-                        </th>
-                        <th>
-                          City
-                        </th>
-                        <th>
-                          Salary
-                        </th>
-                      </tr></thead>
-                      <tbody>
-                        <tr>
-                          <td>
-                            1
-                          </td>
-                          <td>
-                            Dakota Rice
-                          </td>
-                          <td>
-                            Niger
-                          </td>
-                          <td>
-                            Oud-Turnhout
-                          </td>
-                          <td class="text-primary">
-                            $36,738
-                          </td>
-                        </tr>
-                        <tr>
-                          <td>
-                            2
-                          </td>
-                          <td>
-                            Minerva Hooper
-                          </td>
-                          <td>
-                            Curaçao
-                          </td>
-                          <td>
-                            Sinaai-Waas
-                          </td>
-                          <td class="text-primary">
-                            $23,789
-                          </td>
-                        </tr>
-                        <tr>
-                          <td>
-                            3
-                          </td>
-                          <td>
-                            Sage Rodriguez
-                          </td>
-                          <td>
-                            Netherlands
-                          </td>
-                          <td>
-                            Baileux
-                          </td>
-                          <td class="text-primary">
-                            $56,142
-                          </td>
-                        </tr>
-                        <tr>
-                          <td>
-                            4
-                          </td>
-                          <td>
-                            Philip Chaney
-                          </td>
-                          <td>
-                            Korea, South
-                          </td>
-                          <td>
-                            Overland Park
-                          </td>
-                          <td class="text-primary">
-                            $38,735
-                          </td>
-                        </tr>
-                        <tr>
-                          <td>
-                            5
-                          </td>
-                          <td>
-                            Doris Greene
-                          </td>
-                          <td>
-                            Malawi
-                          </td>
-                          <td>
-                            Feldkirchen in Kärnten
-                          </td>
-                          <td class="text-primary">
-                            $63,542
-                          </td>
-                        </tr>
-                        <tr>
-                          <td>
-                            6
-                          </td>
-                          <td>
-                            Mason Porter
-                          </td>
-                          <td>
-                            Chile
-                          </td>
-                          <td>
-                            Gloucester
-                          </td>
-                          <td class="text-primary">
-                            $78,615
-                          </td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </div>
-                </div>
-              </div>
-            </div>
-</div>
-        </div>
-      </div>
+<table class="table table-bordered table-striped" id="datatable-default">
+    <thead>
+        <tr>
+            <th>Semester</th>
+            <th>Kode MTK</th>
+            <th>Nama Mata Kuliah</th>
+            <th>Status TM</th>
+            <th>Kode Bahan Ajar</th>
+        </tr>
+    </thead>
+    @if(!empty($caripaketsemester))
+    <tbody>
+        @foreach($caripaketsemester as $key => $row)
+        <tr class="gradeX">
+            <td><center><?php echo $row->semester ?></center></td>
+            <td><center><?php echo $row->kode_mtk ?></center></td>
+            <td><center><?php echo $row->nama_mtk ?></center></td>
+            <td><center><?php echo $row->status_tm ?></center></td>
+            <td><center><?php echo $row->kode_bahan_ajar ?></center></td>
+        </tr>
+        @endforeach
+    </tbody>
+    @else
+    <tbody>
+        <tr class="gradeX">
+            <td><center>&nbsp;</center></td>
+            <td><center>&nbsp;</center></td>
+            <td><center>&nbsp;</center></td>
+            <td><center>&nbsp;</center></td>
+            <td><center>&nbsp;</center></td>
+        </tr>
+    </tbody>
+    @endif
+</table>
 @stop
